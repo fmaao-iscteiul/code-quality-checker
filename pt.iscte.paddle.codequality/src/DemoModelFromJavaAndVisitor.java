c8f7f6facf1accf0ef77f5676b4a4464a2e28111
André Santos <andresantos@MacBook-Pro.lan>
2020-01-01 17:10:24 +0000

    minor change in demo

--- pt.iscte.paddle.codequality/src/DemoModelFromJavaAndVisitor.java
+++ pt.iscte.paddle.codequality/src/DemoModelFromJavaAndVisitor.java
@@ -1,17 +1,11 @@
 import java.io.File;
-import java.util.ArrayList;
 
+import pt.iscte.paddle.codequality.cases.EmptyCondition;
 import pt.iscte.paddle.interpreter.ExecutionError;
-import pt.iscte.paddle.javali.translator.ElementLocation;
-import pt.iscte.paddle.javali.translator.Model2Java;
 import pt.iscte.paddle.javali.translator.Translator;
-import pt.iscte.paddle.model.IBlock.IVisitor;
-import pt.iscte.paddle.model.IBlock;
-import pt.iscte.paddle.model.IBreak;
+import pt.iscte.paddle.model.IModel2CodeTranslator;
 import pt.iscte.paddle.model.IModule;
 import pt.iscte.paddle.model.IProcedure;
-import pt.iscte.paddle.model.ISelection;
-import pt.iscte.paddle.codequality.cases.*;
 
 public class DemoModelFromJavaAndVisitor {
 
@@ -23,7 +17,7 @@ public class DemoModelFromJavaAndVisitor {
 		IProcedure nats = module.getProcedures().iterator().next(); // first procedure
 
 		// prints model as code
-		String src = module.translate(new Model2Java());
+		String src = module.translate(new IModel2CodeTranslator.Java());
 //		System.out.println(src);
 
 		// Model visitor
