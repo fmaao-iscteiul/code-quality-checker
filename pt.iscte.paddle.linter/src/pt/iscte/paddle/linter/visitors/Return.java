e5add1f8c598e6c638da36c34dc538023a80ca96
Francisco Alfredo <falfredo@visor.ai>
2021-01-06 23:59:41 +0000

    Useless_boolean_map fix

--- pt.iscte.paddle.linter/src/pt/iscte/paddle/linter/visitors/Return.java
+++ pt.iscte.paddle.linter/src/pt/iscte/paddle/linter/visitors/Return.java
@@ -1,16 +1,13 @@
 package pt.iscte.paddle.linter.visitors;
 
-import java.util.Set;
-
 import pt.iscte.paddle.linter.cases.base.CodeAnalyser;
-import pt.iscte.paddle.linter.cases.base.QualityIssue;
 import pt.iscte.paddle.linter.issues.BooleanReturnCheck;
 import pt.iscte.paddle.linter.issues.UselessReturn;
-import pt.iscte.paddle.linter.linter.Linter;
 import pt.iscte.paddle.linter.misc.BadCodeAnalyser;
 import pt.iscte.paddle.model.IControlStructure;
 import pt.iscte.paddle.model.IProcedure;
 import pt.iscte.paddle.model.IReturn;
+import pt.iscte.paddle.model.ISelection;
 import pt.iscte.paddle.model.IType;
 import pt.iscte.paddle.model.cfg.IBranchNode;
 import pt.iscte.paddle.model.cfg.IControlFlowGraph;
@@ -32,25 +29,30 @@ public class Return extends CodeAnalyser implements BadCodeAnalyser {
 		}
 		for (INode node : cfg.getNodes()) {
 
-//			if(node.getElement() != null && node.getElement() instanceof IReturn) {
-//				IReturn ret = (IReturn) node.getElement();
-//
-//				if(ret.getOwnerProcedure().getReturnType().equals(IType.VOID)
-//						&& ret.getOwnerProcedure().getBlock().getChildren().contains(ret))
-//					issues.add(new UselessReturn(ret.getOwnerProcedure(), ret));
-//			}
+			//			if(node.getElement() != null && node.getElement() instanceof IReturn) {
+			//				IReturn ret = (IReturn) node.getElement();
+			//
+			//				if(ret.getOwnerProcedure().getReturnType().equals(IType.VOID)
+			//						&& ret.getOwnerProcedure().getBlock().getChildren().contains(ret))
+			//					issues.add(new UselessReturn(ret.getOwnerProcedure(), ret));
+			//			}
 
-			if(node instanceof IBranchNode 
-					&& ((IBranchNode) node).hasBranch() 
-					&& ((IBranchNode) node).getAlternative().getElement() instanceof IReturn) {
-				IReturn ret = (IReturn) ((IBranchNode) node).getAlternative().getElement();
+			if(node instanceof IBranchNode && ((IBranchNode) node).hasBranch() ) {
 				IControlStructure s = node.getElement().getProperty(IControlStructure.class);
-				if(s.getGuard().getType().equals(IType.BOOLEAN) 
-						&& (ret.getExpression() != null && ret.getExpression().isSame(IType.BOOLEAN.literal(true))
-						|| ret.getReturnValueType().isSame(IType.BOOLEAN.literal(false)))) {
-					issues.add(new BooleanReturnCheck(getProcedure(), s));
+				if(s != null && s.getGuard().getType().equals(IType.BOOLEAN) && 
+						s instanceof ISelection && ((ISelection) s).hasAlternativeBlock() 
+						&& ((IBranchNode) node).getNext().getElement() instanceof IReturn 
+						&& ((IBranchNode) node).getAlternative().getElement() instanceof IReturn) {
+					IReturn altRet = (IReturn) ((IBranchNode) node).getAlternative().getElement();
+					IReturn nextRet = (IReturn) ((IBranchNode) node).getNext().getElement();
+					if((altRet.getExpression() != null && altRet.getExpression().isSame(IType.BOOLEAN.literal(true))
+							&& nextRet.getExpression() != null && nextRet.getExpression().isSame(IType.BOOLEAN.literal(false)))
+							//							|| (altRet.getExpression() != null && altRet.getExpression().isSame(IType.BOOLEAN.literal(false))
+							//							&& nextRet.getExpression() != null && nextRet.getExpression().isSame(IType.BOOLEAN.literal(true)))
+							) {
+						issues.add(new BooleanReturnCheck(getProcedure(), s));
+					}
 				}
-
 			}
 		}
 
